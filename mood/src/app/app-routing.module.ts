bbdc4dc32a7059e5562dad44993ef02b65deaed6
veh0 <valentinohleyer@live.fr>
2022-03-18 11:19:02 +0100

    router fonctionnel

--- mood/src/app/app-routing.module.ts
+++ mood/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvitationsComponent } from './components/invitations/invitations.component';
+import { GroupsComponent } from './layouts/groups/groups.component';
+import { HomepageComponent } from './layouts/homepage/homepage.component';
+import { InscriptionComponent } from './layouts/inscription/inscription.component';
+import { LoginComponent } from './layouts/login/login.component';
+import { ProfileComponent } from './layouts/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomepageComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sigin',
+    component: InscriptionComponent
+  },
+  {
+    path: 'groups',
+    component: GroupsComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'invitations',
+    component: InvitationsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
