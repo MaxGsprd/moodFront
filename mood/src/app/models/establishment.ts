a40d76eef248eb3c9804e9e3c7b3c85607bf02cc
Max <max.gaspard@outlook.fr>
2022-04-10 19:30:41 +0200

    add establishment model interface

---
+++ mood/src/app/models/establishment.ts
@@ -0,0 +1,10 @@
+
+export interface Establishment {
+    name: string;
+    description: string;
+    // address: Address;
+    // images: Images;
+    note: number;
+    comments: Array<Comment>;
+    // category: Category;
+}
