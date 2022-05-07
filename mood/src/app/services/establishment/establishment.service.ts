80148eb260588e3c128c32f4e45835e9195a7f3f
Max <max.gaspard@outlook.fr>
2022-05-07 15:44:56 +0200

    fix service method urls

--- mood/src/app/services/establishment/establishment.service.ts
+++ mood/src/app/services/establishment/establishment.service.ts
@@ -26,30 +26,30 @@ export class EstablishmentService {
   }
 
   public getEstablishment(establishmentId: number): Observable<EstablishmentDetails> {
-    return this.http.get<EstablishmentDetails>(`${this.url}/establishments/${establishmentId}`);
+    return this.http.get<EstablishmentDetails>(`${this.url}establishment/${establishmentId}`);
   }
 
   public getEstablishmentsByName(establishmentName: string): Observable<EstablishmentDetails[]> {
-    return this.http.get<EstablishmentDetails[]>(`${this.url}/establishments/${establishmentName}`);
+    return this.http.get<EstablishmentDetails[]>(`${this.url}establishments/${establishmentName}`);
   }
 
   public getEstablishmentsByCategory(categoryId: number): Observable<EstablishmentDetails[]> {
-    return this.http.get<EstablishmentDetails[]>(`${this.url}/establishments/category${categoryId}`);
+    return this.http.get<EstablishmentDetails[]>(`${this.url}establishments/category${categoryId}`);
   }
 
   public getEstablishmentsByStatus(status: number): Observable<EstablishmentDetails[]> {
-    return this.http.get<EstablishmentDetails[]>(`${this.url}/establishmentsByStatus/${status}`);
+    return this.http.get<EstablishmentDetails[]>(`${this.url}establishmentsByStatus/${status}`);
   }
 
   public addEstablishment(establishment: EstablishmentForm): Observable<EstablishmentForm> {
-    return this.http.post<EstablishmentForm>(`${this.url}/newEstablishment`, establishment);
+    return this.http.post<EstablishmentForm>(`${this.url}newEstablishment`, establishment);
   }
 
   public updateEstablishment(establishment: EstablishmentForm): Observable<EstablishmentForm> {
-    return this.http.put<EstablishmentForm>(`${this.url}/establishment/${establishment}`, establishment);
+    return this.http.put<EstablishmentForm>(`${this.url}establishment/${establishment}`, establishment);
   }
 
   public deleteEstablishment(establishmentId: number): Observable<void> {
-    return this.http.delete<void>(`${this.url}/establishment/${establishmentId}`);
+    return this.http.delete<void>(`${this.url}establishment/${establishmentId}`);
   }
 }
