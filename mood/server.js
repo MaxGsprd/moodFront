f82843bad6418a592ebed46544734b3dad36e253
Max <max.gaspard@outlook.fr>
2022-05-28 14:27:54 +0200

    modify app name

--- mood/server.js
+++ mood/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/mood-aston-app'));
+app.use(express.static('./dist/mood'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mood-aston-app/'}),
+    res.sendFile('index.html', {root: 'dist/mood/'}),
 );
 
 // Start the app by listening on the default Heroku port
